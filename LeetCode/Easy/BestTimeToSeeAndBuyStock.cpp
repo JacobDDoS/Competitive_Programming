e8d8ce1ea31bdcaf041bd5579828e893ef33d6e7
Jacob <jacdimmitt@gmail.com>
2023-09-13 22:31:36 -0500

    Updated LC solution to use constant memory

--- LeetCode/Easy/BestTimeToSeeAndBuyStock.cpp
+++ LeetCode/Easy/BestTimeToSeeAndBuyStock.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        vector<int> lowestAtEachPoint(prices.size());
-        int lowest = 999999;
+        int lowest = 999999, maxProfit = 0;
         for (int i=0;i<prices.size();i++) {
             lowest = std::min(lowest, prices[i]);
-            lowestAtEachPoint[i] = lowest;
-        }
-        int maxProfit = 0;
-        for (int i=0;i<prices.size();i++) {
-            maxProfit = std::max(maxProfit, (prices[i]-lowestAtEachPoint[i]));
+            maxProfit = std::max(maxProfit, (prices[i]-lowest));
         }
         return maxProfit;
     }
